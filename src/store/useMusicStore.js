b890513e130881cf4b09232fc5d43d9d899ef7d3
noraz <haoyu039@gmail.com>
2026-07-08 03:23:07 +0800

    refactor: remove music store constants shim

--- src/store/useMusicStore.js
+++ src/store/useMusicStore.js
@@ -1,18 +1,4 @@
 import { create } from 'zustand';
-import {
-  BEATS_PER_BAR,
-  CHORD_SPAN,
-  CORE_TRACK_IDS,
-  DEFAULT_BPM,
-  DRUMS_INSTRUMENT_IDS,
-  EIGHTH_STEPS_PER_BAR,
-  OPTIONAL_TRACK_IDS,
-  ROOT_KEY,
-  SCALE,
-  STEPS_PER_BAR,
-  TOTAL_BARS,
-  TRACK_IDS,
-} from '../domain/musicConstants.js';
 import createContextSlice from './slices/contextSlice.js';
 import createClipsSlice from './slices/clipsSlice.js';
 import createMatrixSlice from './slices/matrixSlice.js';
@@ -24,20 +10,5 @@ const useMusicStore = create((set, get) => ({
   ...createContextSlice(set, get),
   ...createClipsSlice(set, get),
 }));
-
-export {
-  BEATS_PER_BAR,
-  CHORD_SPAN,
-  CORE_TRACK_IDS,
-  DEFAULT_BPM,
-  DRUMS_INSTRUMENT_IDS,
-  EIGHTH_STEPS_PER_BAR,
-  OPTIONAL_TRACK_IDS,
-  ROOT_KEY,
-  SCALE,
-  STEPS_PER_BAR,
-  TOTAL_BARS,
-  TRACK_IDS,
-};
 
 export default useMusicStore;
